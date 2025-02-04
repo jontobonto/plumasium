72a3e1914ad338c88576c2dea49343747ecd4249
Jonte Wiemers <jo25bu@gmail.com>
2025-02-04 14:08:21 +0100

    game logic

--- dobble/dobble.py
+++ dobble/dobble.py
@@ -1,14 +1,31 @@
-import logging 
-from typing import TYPE_CHECKING
+import logging
+from typing import TYPE_CHECKING, Optional
+
+from . import utils
 
 import discord
 from redbot.core import Config, app_commands, commands
 
+from .utils import I
+
 if TYPE_CHECKING:
     from redbot.core.bot import Red
 
+
 log = logging.getLogger("dobble")
+
 
 class Dobble(commands.Cog):
     def __init__(self, bot: "Red"):
         self.bot = bot
+
+    @property
+    def _utils(self):
+        return utils
+
+    @app_commands.command(name="dobble")
+    async def _dobble(
+        self,
+        interaction: I,
+    ):
+        "Start a Dobble game with up to 8 players, including yourself."
--- dobble/gen_cards/gen_cards.py
+++ dobble/gen_cards/gen_cards.py
@@ -1,18 +1,26 @@
-import random
-import logging
-from typing import TYPE_CHECKING, Optional
+symbols = "⚓ 🍎 👁️ ❗ 🚙 🌳 🍃 ⚡ 💐 💣 🤡 🐬 🦕 👋 🐉 🧊 🔥 🍾 ❓ 👻 💡 🔨 ❤️ 🐶 ⛺ 🌵 🧀 🐱 🕯️ 🖌️ 🍀 👄 🙋 🐞 🥕 🌙 🎵 🐴 ✂️ ⛔ 🐢 🔒 🗝️ ❄️ ⛄ ☀️ 🕶️ 🕷️ 🕸️ 🖊️ 💀 🕰️ 🐦 💧 ☯️ 🦓 🏁".split(
+    " "
+)
+print(symbols)
+print(len(symbols))
 
-from . import utils
+import csv, pathlib, random
 
-import discord
-from redbot.core import Config, app_commands, commands
+cards = [[] for _ in range(57)]
+# print(cards)
 
-if TYPE_CHECKING:
-    from redbot.core.bot import Red
+with open(pathlib.Path("dobble\gen_cards\Dobble_2.csv")) as file:
+    reader = csv.reader(file)
+    for index, row in enumerate(reader):
+        if index == 0:
+            continue
+        for card_index, symbol in enumerate(row):
+            if symbol.startswith("Symbol"):
+                continue
+            if symbol == "1":
+                cards[card_index - 1].append(symbols[index - 1])
 
-I = discord.Interaction["Red"]
-
-_cards = [
+cards = [
     ["⚡", "🐬", "🔥", "🐶", "🥕", "🌙", "🎵", "🖊️"],
     ["🍃", "❓", "🔒", "⛄", "🕷️", "🖊️", "💀", "💧"],
     ["❗", "👋", "🔥", "🕯️", "🙋", "🐢", "🕷️", "🦓"],
@@ -71,58 +79,3 @@ _cards = [
     ["⚓", "👋", "🧊", "❓", "🔨", "🐱", "🥕", "❄️"],
     ["❗", "🚙", "💡", "❤️", "🌵", "🌙", "❄️", "💀"],
 ]
-
-
-class Colour(discord.Colour):
-    @classmethod
-    def dobble(cls):
-        """A factory method that returns a :class:`Colour` with a value of ``0x9A73A9``.
-
-        .. colour:: #9A73A9
-
-        .. versionadded:: 2.3
-        """
-        return cls(0x9A73A9)
-
-
-class Game:
-    def __init__(self, players: list[discord.Member], cards: list[list[str]], thread: discord.Thread):
-        self.players = players
-        self.cards = cards
-        self.thread = thread
-
-    async def add_player(self, player: discord.Member):
-        self.players.append(player)
-        await self.thread.add_user(player)
-
-    @classmethod
-    async def start(cls, interaction: I):
-        cards = _cards.copy()
-        random.shuffle(cards)
-        thread = await interaction.channel.create_thread(name="Dobble")
-        game = cls(
-            players=[
-                interaction.user,
-            ],
-            cards=cards,
-            thread=thread,
-        )
-
-        join_game_view = JoinGameView(game)
-        embed = discord.Embed(colour=Colour.dobble(), title="Dobble")
-        await interaction.response.send_message(embed=embed, view=join_game_view)
-
-
-class JoinGameView(discord.ui.View):
-    def __init__(self, game: Game):
-        super().__init__(timeout=60)
-
-        self.game = game
-
-    @discord.ui.button(
-        label="Join Game",
-        style=discord.ButtonStyle.grey,
-    )
-    async def join_game(self, interaction: I, button: discord.ui.Button):
-        await self.game.add_player(interaction.user)
-        await interaction.response.defer()
